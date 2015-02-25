bee70ba2e25b97832a3436e559575d4d05d568d9
Tricia Buckenberger <tbuckenberger@gmail.com>
2015-02-25 15:47:07 -0800

    Authentication added

--- routes/makeupsroutes.js
+++ routes/makeupsroutes.js
@@ -1,20 +1,23 @@
 'use strict';
 
 var Makeup = require('../models/Makeup');
+var eat_auth = require('../lib/eat_auth');
 var bodyparser = require('body-parser');
 
-module.exports = function(app) {
+module.exports = function(app, appSecret) {
   app.use(bodyparser.json());
 
-  app.get('/makeups', function(req, res) {
+  app.get('/makeups', eat_auth(appSecret), function(req, res) {
     Makeup.find({}, function(err, data) {
+      console.log(req.body);
+    // Makeup.find({user_id: req.user._id}, function(err, data) {
       if (err) return res.status(500).send({'msg': 'could not retrieve makeup'});
 
       res.json(data);
     });
   });
 
-  app.post('/makeups', function(req, res) {
+  app.post('/makeups', eat_auth(appSecret), function(req, res) {
     var newMakeup = new Makeup(req.body);
     newMakeup.save(function(err, makeup) {
       if (err) return res.status(500).send({'msg': 'could not save makeup'});
@@ -23,7 +26,7 @@ module.exports = function(app) {
     });
   });
 
-  app.put('/makeups/:id', function(req, res) {
+  app.put('/makeups/:id', eat_auth(appSecret), function(req, res) {
     var updatedMakeup = req.body;
     delete updatedMakeup._id;
     Makeup.update({_id: req.params.id}, updatedMakeup, function(err) {
@@ -33,7 +36,7 @@ module.exports = function(app) {
     });
   });
 
-  app.delete('/makeups/:id', function(req, res) {
+  app.delete('/makeups/:id', eat_auth(appSecret), function(req, res) {
     Makeup.remove({_id: req.params.id}, function(err) {
       if (err) return res.status(500).send({'msg': 'could not delete makeup'});
 
